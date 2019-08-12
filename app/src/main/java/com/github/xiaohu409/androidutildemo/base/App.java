b0effb117fbb0da8883fafa4592ac529805fa6bb
胡涛 <285279904@qq.com>
2019-08-12 15:40:55 +0800

    1.重构App.java

--- app/src/main/java/com/github/xiaohu409/androidutildemo/base/App.java
+++ app/src/main/java/com/github/xiaohu409/androidutildemo/base/App.java
@@ -2,8 +2,10 @@ package com.github.xiaohu409.androidutildemo.base;
 
 import android.app.Application;
 
+import com.github.xiaohu409.androidutil.LogUtil;
 import com.github.xiaohu409.androidutil.SharePreUtil;
 import com.github.xiaohu409.androidutil.ToastUtil;
+import com.github.xiaohu409.androidutildemo.BuildConfig;
 
 /**
  * 项目名称：ToolUtilDemo
@@ -18,15 +20,17 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        //初始化
         ToastUtil.initToastUtil(this);
         SharePreUtil.initSharePreUtil(this, "test.xml");
+        LogUtil.setDebug(BuildConfig.DEBUG);
     }
 
     @Override
     public void onTerminate() {
         super.onTerminate();
+        //释放引用
         ToastUtil.releaseContext();
         SharePreUtil.release();
     }
-
 }
