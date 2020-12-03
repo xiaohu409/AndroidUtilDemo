d4e007f4cc6458ecde3e0d9e520f17e43e53a7b0
hutao <285279904@qq.com>
2020-12-03 08:43:19 +0800

    1.提交

---
+++ app/src/main/java/com/github/xiaohu409/androidutildemo/bean/A.java
@@ -0,0 +1,19 @@
+package com.github.xiaohu409.androidutildemo.bean;
+
+public class A {
+
+
+    /**
+     * key : 12
+     */
+
+    private int key;
+
+    public int getKey() {
+        return key;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+}
